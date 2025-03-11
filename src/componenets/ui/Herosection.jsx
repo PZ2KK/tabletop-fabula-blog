73f95eceeb9e92387f05cf6a6f6863eed60ec818
Pom <kasiphit@gmail.com>
2025-03-11 20:50:48 +0700

    feat:Add hero section layout

---
+++ src/componenets/ui/Herosection.jsx
@@ -0,0 +1,51 @@
+function HeroSection() {
+    return (
+        <div className="md:flex-row  md:mx-[140px] md:py-40 flex flex-col items-center justify-between py-20 p-6">
+
+            {/* Left box */}
+            <div className="md:w-1/3 md:text-right md:items-end w-full flex flex-col items-center mb-6 ">
+                <h1 className="md:text-5xl text-4xl font-bold mb-6">
+                    This is your <span className="text-[#02695A] md:text-7xl">w</span>orld, 
+                    <br />
+                    this is your story 
+                </h1>
+                <p className="md:text-2xl text-gray-600 text-md">
+                    Made your choice and fight for what you believe for.
+                </p>
+            </div>
+
+            {/* Middle box */}
+            <div className="flex justify-center relative">
+                <a href="https://www.drivethrurpg.com/en/product/410108/fabula-ultima-ttjrpg" target="_blank" className="block w-full">
+                    <div className="mx-auto w-[70%] hover:scale-110 hover:brightness-120 transform transition-all duration-300">
+                        <img className="rounded-3xl overflow-hidden cursor-pointer w-full"
+                                src="src\assets\images\banners\bookcover.jpg"
+                                alt="Fabula Ultima Volume 1 Bookcover"
+                            /> 
+                    </div>
+                </a>
+            </div>
+            
+
+            {/* Right box */}
+            <div className="md:w-1/3 w-full flex flex-col items-start p-6 rounded-lg">
+                <h2 className="text-2xl font-semibold mb-6">
+                    FABULA ULTIMA
+                </h2>
+                <p className="text-gray-600 text-md mb-3">
+                    is a Tabletop Roleplaying Game inspired by <span className="text-[black] font-bold">Japanese-style console RPGs,</span> or JRPGs. In Fabula Ultima, you and your friends will tell epic stories of would-be heroes and fearsome villains, set in fantasy worlds brimming with magic, wondrous locations, and uniquely bizarre monsters!
+                </p>
+                <ol className="list-disc pl-5 text-gray-600">
+                    <li>Create your own setting together</li>
+                    <li>A simple and intuitive ruleset</li>
+                    <li>A Bestiary</li>
+                    <li>Fifteen Classes to mix and match</li>
+                    <li>Everything the Game Master needs</li>
+                    <li>360 full-color pages</li>
+                </ol>
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection;
